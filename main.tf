3e413c53b61ef341b33f5ee24c1c2b23f2301d0e
Tanveer143s <ertanveer143@gmail.com>
2024-06-21 23:45:13 +0530

    feat: updated main.tf

--- main.tf
+++ main.tf
@@ -100,3 +100,48 @@ resource "google_dns_managed_zone" "public" {
     enable_logging = var.enable_logging
   }
 }
+
+
+resource "google_dns_record_set" "cloud-static-records" {
+  project      = var.project_id
+  managed_zone = var.name
+
+  for_each = { for record in var.recordsets : join("/", [record.name, record.type]) => record }
+  name = (
+    each.value.name != "" ?
+    "${each.value.name}.${var.domain}" :
+    var.domain
+  )
+  type = each.value.type
+  ttl  = each.value.ttl
+
+  rrdatas = each.value.records
+
+  dynamic "routing_policy" {
+    for_each = toset(each.value.routing_policy != null ? ["create"] : [])
+    content {
+      dynamic "wrr" {
+        for_each = each.value.routing_policy.wrr
+        iterator = wrr
+        content {
+          weight  = wrr.value.weight
+          rrdatas = wrr.value.records
+        }
+      }
+
+      dynamic "geo" {
+        for_each = each.value.routing_policy.geo
+        iterator = geo
+        content {
+          location = geo.value.location
+          rrdatas  = geo.value.records
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    google_dns_managed_zone.private,
+    google_dns_managed_zone.public,
+  ]
+}
